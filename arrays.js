e963d0a624c23731a1137a0d84537ff6633f824d
Morganne Gagne <morgannegagne@gmail.com>
2017-11-05 20:10:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element);
+  return array = array.push(element);
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
